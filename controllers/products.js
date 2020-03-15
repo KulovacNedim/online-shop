21a313d78ad73edfe7511e83f6be6e8f279fd7e5
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-15 20:08:07 +0100

    products.json file created to serve as .

--- controllers/products.js
+++ controllers/products.js
@@ -17,13 +17,15 @@ exports.postAddProducts = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
+
 };
