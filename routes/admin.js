ac9f2ee3bbf06b977323b0d4941dc6085cd9017e
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-15 21:31:23 +0100

    shop structure (models, controllers, views) created

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct);
-
-// /admin/add-product => POST
-router.post('/add-product', productsController.postAddProducts);
+router.get('/add-product', adminController.getAddProduct);
+router.post('/add-product', adminController.postAddProducts);
+router.get('/products', adminController.getProducts);
 
 module.exports = router;
