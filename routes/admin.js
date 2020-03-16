15dbc4508152d651c33ff8c6dbeb6b527ff46ef0
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-16 22:31:30 +0100

    Editing products implemented as well as saving newly created products. Adding products to cart also implemented.

--- routes/admin.js
+++ routes/admin.js
@@ -9,5 +9,7 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProducts);
 router.get('/products', adminController.getProducts);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProducts);
 
 module.exports = router;
