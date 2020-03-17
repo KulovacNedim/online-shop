a2a2287bd13c297321263520e6b91721cbf04110
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-17 14:50:16 +0100

    deleting items implemented and cart logic finished.

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,11 +8,11 @@ exports.getAddProduct = (req, res, next) => {
     });
 };
 
-exports.postAddProducts = (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
-    const description = req.body.description;
     const price = req.body.price;
+    const description = req.body.description;
     const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
@@ -37,23 +37,35 @@ exports.getEditProduct = (req, res, next) => {
     });
 };
 
-exports.postEditProducts = (req, res, next) => {
+exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    const updatedProduct = new Product(
+        prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedDesc,
+        updatedPrice
+    );
     updatedProduct.save();
     res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
+    Product.fetchAll(products => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
             path: '/admin/products'
         });
     });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
 };
--- routes/admin.js
+++ routes/admin.js
@@ -7,9 +7,10 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
-router.post('/add-product', adminController.postAddProducts);
 router.get('/products', adminController.getProducts);
+router.post('/add-product', adminController.postAddProduct);
 router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product', adminController.postEditProducts);
+router.post('/edit-product', adminController.postEditProduct);
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
