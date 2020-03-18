8bc0b77f8525d5883a5943be11664a70323ef7ff
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-18 12:45:42 +0100

    User model defined and associated with product model. Controllers adapted to use new setup.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,12 +19,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-sequelize.sync()
-    .then(() => {
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+sequelize
+    // .sync({force: true})
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({name: 'Nedim', email: 'dummy@email.com'});
+        }
+        return user;
+    })
+    .then(user => {
+        console.log(user)
         app.listen(3000);
     })
     .catch(err => console.log(err));
