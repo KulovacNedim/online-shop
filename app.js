eb72ebc0dd4ee9b23a68f28369c4cc7d43e4de22
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-20 09:35:24 +0100

    All cart in user model and shop controller related logic converted to follow mondodb best practice

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('5e738a3b91683a16c08219a6')
       .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
       })
       .catch(err => console.log(err));
