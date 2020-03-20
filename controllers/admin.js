be9b865f9a2ea3413465093f61c589d7a6d96612
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-20 23:06:38 +0100

    dummy authentication implemented. session enabled and mongodb stores it

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,8 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false
+        editing: false,
+        isAuthenticated: req.session.isLoggedIn
     });
 };
 
@@ -47,7 +48,8 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product
+                product: product,
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -66,10 +68,10 @@ exports.postEditProduct = (req, res, next) => {
             product.price = updatedPrice;
             product.description = updatedDesc;
             product.imageUrl = updatedImageUrl;
-            return product
-                .save()
+            return product.save();
         })
         .then(result => {
+            console.log('UPDATED PRODUCT!');
             res.redirect('/admin/products');
         })
         .catch(err => console.log(err));
@@ -77,12 +79,15 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.find()
-        // .populate('userId')
+    // .select('title price -_id')
+    // .populate('userId', 'name')
         .then(products => {
+            console.log(products);
             res.render('admin/products', {
                 prods: products,
                 pageTitle: 'Admin Products',
-                path: '/admin/products'
+                path: '/admin/products',
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => console.log(err));
@@ -92,6 +97,7 @@ exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     Product.findByIdAndRemove(prodId)
         .then(() => {
+            console.log('DESTROYED PRODUCT');
             res.redirect('/admin/products');
         })
         .catch(err => console.log(err));
