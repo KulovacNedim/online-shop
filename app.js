90a4e4aa74ec29a18af156bbd64de6d77c7c4f50
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-20 12:15:48 +0100

    mongoose installed. product model and admin controller adjusted to support CRUD operations over product model

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -15,23 +15,28 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('5e738a3b91683a16c08219a6')
-      .then(user => {
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-      })
-      .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById('5e738a3b91683a16c08219a6')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
+mongoose
+    .connect('mongodb+srv://root:toor@cluster0-muewd.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
