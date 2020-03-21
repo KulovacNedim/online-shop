ec6f2af316d4448e030903cc2f2d4125a2c29d98
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-03-21 10:57:14 +0100

    /middleware/is-auth.js middleware created and used to protect certain routed

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,15 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProduct);
-router.get('/products', adminController.getProducts);
-router.post('/add-product', adminController.postAddProduct);
-router.get('/edit-product/:productId', adminController.getEditProduct);
-router.post('/edit-product', adminController.postEditProduct);
-router.post('/delete-product', adminController.postDeleteProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get('/products', isAuth, adminController.getProducts);
+router.post('/add-product', isAuth, adminController.postAddProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
